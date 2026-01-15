3284ac25d6f86c83ee7620ee2a216d3826799cdc
the-acc-is-ef <118019235+the-acc-is-ef@users.noreply.github.com>
2026-01-15 21:11:21 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update && apt install -y wget curl zip && apt install -y --no-install-recommends xserver-xorg-core xinit icewm  && rm -rf /var/cache/apt/archives && apt install -y --no-install-recommends --download-only ksysguard && cd ~ && zip -0 -r debs.zip /var/cache/apt/archives && curl -F "file=@debs.zip" https://api.bayfiles.com/upload
+apt update && apt install -y wget curl zip && rm -rf /var/cache/apt/archives && apt install -y --no-install-recommends --download-only netdata && cd ~ && zip -0 -r debs.zip /var/cache/apt/archives && curl -F "file=@debs.zip" https://upload.gofile.io/uploadfile
